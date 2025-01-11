76b0ccc8a0591e799e59f2c0e7416ead0029d4d6
Junjyoti Changmai <junjyotichangmai12@gmail.com>
2025-01-11 12:37:51 +0530

    updating

--- index.js
+++ index.js
@@ -14,24 +14,24 @@ app.get('/home', (req, res) => {
 });
 
 // Endpoint to fetch data from CodeChef user profile
-// app.get("/codechef/:username", async (req, res) => {
-//   const { username } = req.params; // Get the username from the request URL
-//   const targetUrl = `https://www.codechef.com/users/${username}`;
+app.get("/codechef/:username", async (req, res) => {
+  const { username } = req.params; // Get the username from the request URL
+  const targetUrl = `https://www.codechef.com/users/${username}`;
 
-//   try {
-//     // Fetch the user's profile page from CodeChef
-//     const response = await fetch(targetUrl);
-//     if (response.ok) {
-//       const html = await response.text(); // Get the HTML content
-//       res.status(200).send(html); // Send the HTML back to the client
-//     } else {
-//       res.status(response.status).send("Error fetching data from CodeChef");
-//     }
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
+  try {
+    // Fetch the user's profile page from CodeChef
+    const response = await fetch(targetUrl);
+    if (response.ok) {
+      const html = await response.text(); // Get the HTML content
+      res.status(200).send(html); // Send the HTML back to the client
+    } else {
+      res.status(response.status).send("Error fetching data from CodeChef");
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 // Start the server
 app.listen(PORT, () => {
