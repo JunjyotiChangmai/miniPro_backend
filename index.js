c430b1361b1801c40ef447aac8823cf7928a5913
Junjyoti Changmai <junjyotichangmai12@gmail.com>
2025-01-11 16:19:36 +0530

    codecehef user data api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
+const jsdom = require("jsdom");
 
+const { JSDOM } = jsdom;
 const app = express();
 const PORT = 3000;
 app.use(cors());
@@ -13,18 +15,34 @@ app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
 });
 
-// Endpoint to fetch data from CodeChef user profile
+// CodeChef user profile
 app.get("/codechef/:username", async (req, res) => {
-  const { username } = req.params; // Get the username from the request URL
+  const { username } = req.params;
   const targetUrl = `https://www.codechef.com/users/${username}`;
+  const codechefAPI = `https://codechef-api.vercel.app/handle/${username}`;
 
   try {
-    // Fetch the user's profile page from CodeChef
     const response = await fetch(targetUrl);
+    const CCProfile = await fetch(codechefAPI).then(response => response.json()).then(data => data);
+    
     if (response.ok) {
-      const html = await response.text(); // Get the HTML content
-      res.status(200).send(html); // Send the HTML back to the client
-    } else {
+      const d = await response.text(); 
+      const data = { data: d };
+      const dom = new JSDOM(data.data);
+      const document = dom.window.document;
+
+      const problemSolvedElement = document.querySelector('.rating-data-section.problems-solved').lastElementChild;
+      const totalProb = problemSolvedElement.innerHTML.split(" ")[3];
+      
+      const newData = {
+          problemSolve: parseInt(totalProb),
+      };
+
+      const codeChefUserData = { ...newData, ...CCProfile };
+
+      res.status(200).json(codeChefUserData);
+    } 
+    else {
       res.status(response.status).send("Error fetching data from CodeChef");
     }
   } catch (error) {
