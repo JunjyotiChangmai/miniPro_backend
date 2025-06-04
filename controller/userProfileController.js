0d0a164854afcc87dd29e7e83ee168c854ce3370
Junjyoti Changmai <junjyotichangmai12@gmail.com>
2025-06-05 01:39:43 +0530

    updating

--- controller/userProfileController.js
+++ controller/userProfileController.js
@@ -101,31 +101,54 @@ async function updateUserCodingProfile(req, res) {
 
         let totalProblemSolved = 0;
 
+        //Score calculation
+        let codechefScore = 0;
+        let codeforcesScore = 0;
+        let leetcodeScore = 0;
+        let gfgScore = 0;
+
         if (userdata.codechefusername) {
             const codechefData = await handleCodeChefData(userdata.codechefusername);
             userdata.codingProfiles.codechef = codechefData;
             totalProblemSolved += codechefData.problemSolved;
+            //score
+            codechefScore = 2*codechefData.problemSolved;
         }
 
         if (userdata.codeforcesusername) {
             const codeforcesData = await handleCodeforcesData(userdata.codeforcesusername);
             userdata.codingProfiles.codeforces = codeforcesData;
             totalProblemSolved += codeforcesData.userInfo[0].problemSolved;
+            //score
+            codeforcesScore = 2*codeforcesData.userInfo[0].problemSolved;
         }
 
         if (userdata.gfgusername) {
             const gfgData = await handleGFGData(userdata.gfgusername);
             userdata.codingProfiles.gfg = gfgData;
             totalProblemSolved += gfgData.userInfo.total_problems_solved;
+
+            const gfgEasyScore = 1*Object.keys(gfgData.userSubmissionsInfo.Easy).length;
+            const gfgMediumScore = 2*Object.keys(gfgData.userSubmissionsInfo.Medium).length;
+            const gfgHardScore = 4*Object.keys(gfgData.userSubmissionsInfo.Hard).length;
+
+            gfgScore = gfgEasyScore + gfgMediumScore + gfgHardScore;
         }
 
         if (userdata.leetcodeusername) {
             const leetcodeData = await handleLeetcodeData(userdata.leetcodeusername);
             userdata.codingProfiles.leetcode = leetcodeData;
             totalProblemSolved += leetcodeData.profile.problemSolved;
+
+            const leetcodeEasyScore = 1*leetcodeData.submitStats.acSubmissionNum[1].count;
+            const leetcodeMediumScore = 2*leetcodeData.submitStats.acSubmissionNum[2].count;
+            const leetcodeHardScore = 4*leetcodeData.submitStats.acSubmissionNum[3].count;
+
+            leetcodeScore = leetcodeEasyScore + leetcodeMediumScore + leetcodeHardScore;
         }
 
         userdata.totalProblemsSolved = totalProblemSolved;
+        userdata.score = codechefScore + codeforcesScore + leetcodeScore + gfgScore;
         
         await userdata.save();
         
